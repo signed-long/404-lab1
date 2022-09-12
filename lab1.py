b070caac1d52b969cf2d7616c675a3ddcb01a979
signed-long <mrl@ualberta.ca>
2022-09-12 17:48:26 -0600

    update this script to get itself from raw gh URL

--- lab1.py
+++ lab1.py
@@ -4,5 +4,5 @@ import requests
 print("requests version: " + requests.__version__)
 
 # curl
-res = requests.get("http://www.google.com")
+res = requests.get("https://raw.githubusercontent.com/signed-long/404-lab1/main/lab1.py")
 print(res.text)
